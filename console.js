98e24f0eb3a0b89618de63947fc755b36347f6bf
Yuresh <yureshmee@gmail.com>
2025-02-08 22:26:59 +0545

    Testing rebase on branch

--- console.js
+++ console.js
@@ -3,3 +3,12 @@ console.log("commit 2");
 console.log("commit 3");
 console.log("commit 4");
 console.log("commit 5");
+console.log("commit 6 test branch");
+console.log("commit 7 test branch commit ahead");
+console.log("commit 8 test branch commit ahead");
+console.log(
+  "commit 8 test branch commit ahead one more to test rebase on branch"
+);
+console.log(
+  "commit 8 test branch commit ahead one more to test rebase on branch"
+);
